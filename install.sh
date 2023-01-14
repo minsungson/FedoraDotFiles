18003dc7a7a73ddd293a8bffb990d9415c8d8f09
Minsung Son <100154286+minsungson@users.noreply.github.com>
2023-01-14 17:26:13 +0000

    howdy config stuff

--- install.sh
+++ install.sh
@@ -63,3 +63,37 @@ while true; do
     * ) echo "invalid";;
   esac
 done
+
+# add to /etc/pam.d/sudo
+# auth       sufficient   pam_python.so /lib64/security/howdy/pam.py
+
+# add to /etc/pam.d/gdm-password
+# auth     [success=done ignore=ignore default=bad] pam_selinux_permit.so
+# auth        sufficient    pam_python.so /lib64/security/howdy/pam.py
+
+chmod o+x /lib64/security/howdy/dlib-data
+
+cat << "EOF" > /home/$(whoami)/howdy.te
+module howdy 1.0;
+
+require {
+    type lib_t;
+    type xdm_t;
+    type v4l_device_t;
+    type sysctl_vm_t;
+    class chr_file map;
+    class file { create getattr open read write };
+    class dir add_name;
+}
+
+#============= xdm_t ==============
+allow xdm_t lib_t:dir add_name;
+allow xdm_t lib_t:file { create write };
+allow xdm_t sysctl_vm_t:file { getattr open read };
+allow xdm_t v4l_device_t:chr_file map;
+
+EOF
+
+checkmodule -M -m -o howdy.mod howdy.te
+semodule_package -o howdy.pp -m howdy.mod
+semodule -i howdy.pp
