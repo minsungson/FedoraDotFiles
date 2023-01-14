7edf8c5b5f83809380cdddf5568ffa62a83b12e3
Minsung Son <100154286+minsungson@users.noreply.github.com>
2023-01-14 14:28:15 +0000

    appearance and auto update packages

---
+++ .appearance.sh
@@ -0,0 +1,14 @@
+# !/bin/zsh
+
+# Change gsettings
+echo "Changing Gnome Appearance"
+org.gnome.desktop.background show-desktop-icons false
+
+# find more settings to change
+# gsettings list-recursively
+
+# Install Gnome Extensions
+https://extensions.gnome.org/extension-data/weatheroclockCleoMenezesJr.github.io.v2.shell-extension.zip #Weather in the Clock
+https://extensions.gnome.org/extension-data/user-themegnome-shell-extensions.gcampax.github.com.v50.shell-extension.zip #User Themes
+https://extensions.gnome.org/extension-data/date-menu-formattermarcinjakubowski.github.com.v8.shell-extension.zip #Date Menu Formatter
+https://extensions.gnome.org/extension-data/bluetooth-quick-connectbjarosze.gmail.com.v30.shell-extension.zip #Bluetooth Quick Connect
--- install.sh
+++ install.sh
@@ -11,6 +11,16 @@ while true; do -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 echo "Install Flatpak and Packages"
 bash "/Users/"$(whoami)"/FedoraDotFiles/.flatpak.sh"
 
+# Change Gnome Appearance
+echo "Changing Gnome Appearance"
+bash "/Users/"$(whoami)"/FedoraDotFiles/.appearance.sh"
+
+# Configure automatic updates
+echo "Configure automatic updates"
+sudo dnf install dnf-automatic -y
+sed -c -i "s/\($"apply_updates = no"*= *\).*/\1$"apply_updates = yes"/" $/etc/dnf/automatic.conf
+sudo systemctl enable --now dnf-automatic.timer
+
 # Install MACchanger
 echo "Randomise MAC Address on Boot"
 sudo dnf install macchanger -y
