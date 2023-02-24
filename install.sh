188ac8c8e38a9af0073f7fe11546eb5cdf2ea2f6
Minsung Son <100154286+minsungson@users.noreply.github.com>
2023-02-24 02:39:36 +0000

    reorder

--- install.sh
+++ install.sh
@@ -21,38 +21,34 @@ sudo dnf install dnf-automatic -y
 sed -c -i "s/\($"apply_updates = no"*= *\).*/\1$"apply_updates = yes"/" $/etc/dnf/automatic.conf
 sudo systemctl enable --now dnf-automatic.timer
 
-# Improve speed of DNF
-echo "Improve speed of DNF"
-sudo sed -i -e '4a\max_parallel_downloads=10' /etc/dnf/dnf.conf
-sudo sed -i -e '5a\fastestmirror=true' /etc/dnf/dnf.conf
-
 # Install Flatpak and Packages
 echo "Install Flatpak and Packages"
 bash "/home/"$(whoami)"/FedoraDotFiles/.flatpak.sh"
 
 # Install DNF Packages
 sudo dnf install firefox -y
-
-# Install VS Code
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 sudo dnf check-update
 sudo dnf install code
-
-# Change Gnome Appearance
-echo "Changing Gnome Appearance"
-bash "/home/"$(whoami)"/FedoraDotFiles/.appearance.sh"
+sudo dnf remove epiphany gnome-calculator gnome-characters gnome-maps gnome-music gnome-photos gnome-remote-desktop gnome-user-docs gnome-user-share gnome-video-effects -y --skip-broken
+sudo dnf remove gnome-shell-extension-applications-menu gnome-shell-extension-window-list gnome-shell-extension-background-logo -y --skip-broken
 
 # Install MACchanger
 echo "Randomise MAC Address on Boot"
 sudo dnf install macchanger -y
 
-# Uninstall unnecessary default apps
-sudo dnf remove epiphany gnome-calculator gnome-characters gnome-maps gnome-music gnome-photos gnome-remote-desktop gnome-user-docs gnome-user-share gnome-video-effects -y --skip-broken
-sudo dnf remove gnome-shell-extension-applications-menu gnome-shell-extension-window-list gnome-shell-extension-background-logo
+# Change Gnome Appearance
+echo "Changing Gnome Appearance"
+bash "/home/"$(whoami)"/FedoraDotFiles/.appearance.sh"
 
 # Install Howdy and drivers for fingerprint
 bash "/home/"$(whoami)"/FedoraDotFiles/.howdy.sh"
+
+# Improve speed of DNF
+echo "Improve speed of DNF"
+sudo sed -i -e '4a\max_parallel_downloads=10' /etc/dnf/dnf.conf
+sudo sed -i -e '5a\fastestmirror=true' /etc/dnf/dnf.conf
 
 # Paywall firewall
 curl -C - --output bypass-paywalls-chrome-master.zip https://github.com/iamadamdev/bypass-paywalls-chrome/archive/master.zip
