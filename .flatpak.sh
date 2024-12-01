219562e7930bf3283a0a8f2fb5c0802c4931db82
Minsung Son <100154286+minsungson@users.noreply.github.com>
2024-12-01 15:47:05 +0000

    Update .flatpak.sh

--- .flatpak.sh
+++ .flatpak.sh
@@ -1,10 +1,10 @@
 # !/bin/zsh
 
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-flatpak install flathub com.discordapp.Discord -y
-flatpak install flathub org.libreoffice.LibreOffice -y
+flatpak install flathub org.onlyoffice.desktopeditors -y
 flatpak install flathub com.stremio.Stremio -y
-flatpak install flathub com.skype.Client -y
-flatpak install flathub net.ankiweb.Anki -y
 flatpak install flathub com.calibre_ebook.calibre -y
-flatpak install flathub org.gnome.Solanum -y
+flatpak install flathub io.freetubeapp.FreeTube -y
+flatpak install flathub sh.cider.Cider -y
+flatpak install flathub org.videolan.VLC -y
+flatpak install flathub com.rtosta.zapzap -y
